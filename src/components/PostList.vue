de26e2ef45f1343a573c9cf2350f79587d36ccaa
AliOgretmen <aliogretmen.ch@gmail.com>
2021-02-25 16:47:38 +0100

    http get request

---
+++ src/components/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <button @click="getPosts">Load List</button>
+        <button @click="deletePosts">Delete List</button>
+        <h3 v-if="errorMsg"> {{errorMsg}}</h3>
+        <div v-for="post in posts" :key="post.id">
+            <h3>
+                {{post.id}} {{post.title}}
+            </h3>
+            <p>{{post.body}}</p>
+            <hr>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+    export default {
+        name: 'PostList',
+        data(){
+            return {
+                posts: [], 
+                errorMsg: '',    
+            }
+        },
+        methods: {
+            getPosts(){
+                axios.get('https://jsonplaceholder.typicode.com/posts')
+                .then((response)=> {
+                    console.log(response.data)
+                    this.posts = response.data
+                })
+                .catch((error) => {
+                    console.log(error)
+                    this.errorMsg = 'Erron on retrieving data'
+                })
+                
+            },
+            deletePosts(){
+                this.posts = []
+            }
+        }
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
